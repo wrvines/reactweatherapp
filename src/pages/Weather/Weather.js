018939f62f7368537643d75c24129a7e90fc68c2
poopysmack <54606838+poopysmack@users.noreply.github.com>
2023-01-10 11:17:42 -0700

    trying to get search to work

--- src/pages/Weather/Weather.js
+++ src/pages/Weather/Weather.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React from "react";
 import CurrentWeather from "../../components/CurrentWeather/CurrentWeather";
 import Forecast from "../../components/Forecast/Forecast";
@@ -7,14 +8,45 @@ function Weather() {
   const apiKey = process.env.REACT_WEATHER_API_KEY;
   const locationBaseUrl = process.env.REACT_GEOCODE_BASE_URL;
 
+  const [location, setLocation] = React.useState("");
+  const [lat, setLat] = React.useState("");
+  const [long, setLong] = React.useState("");
+
   const handleSearch = (e) => {
     e.preventDefault();
-    console.log(e);
+    // console.log(e.target.value);
+    setLocation(e.target.value);
   };
+
+  const handleLocation = (e) => {
+    e.preventDefault();
+    //http: api.openweathermap.org/geo/1.0/direct?q=casper&appid=3bc574a245ec78a822b5ac520c18060d
+    // console.log(e.target.value);
+    axios
+      .get(`${locationBaseUrl}?q=${location}&appid=${apiKey}`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  //   axios
+  //     .get(`${locationBaseUrl}?q=${location}&appid=${apiKey}`)
+  //     .then((res) => {
+  //       console.log(res);
+  //     })
+  //     .catch((err) => console.log(err));
 
   return (
     <div>
-      <input onChange={handleSearch} placeholder="Enter Zipcode or City" />
+      <form onSubmit={handleLocation}>
+        <input
+          onChange={handleSearch}
+          placeholder="Enter Zipcode or City"
+          value={location}
+        />
+        <input type="submit" value="Submit" />
+      </form>
       <CurrentWeather />
       <Forecast />
     </div>
